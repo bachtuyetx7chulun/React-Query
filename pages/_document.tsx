c069476348a20ebcab28148f626f8c9b1e67b872
DESKTOP-3FRC7CU\FABBI <fabbi@fabbi.io>
2021-11-30 17:51:41 +0700

    chore: add react-query

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-
+import { CssBaseline } from '@nextui-org/react'
 class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx)
+    const styles = CssBaseline.flush()
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {styles}
+        </>
+      ),
+    }
+  }
+
   render() {
     return (
       <Html>
